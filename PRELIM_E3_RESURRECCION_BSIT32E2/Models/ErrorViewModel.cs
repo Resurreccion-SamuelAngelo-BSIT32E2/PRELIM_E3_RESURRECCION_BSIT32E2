8a6e19415589422e0e06e36b6a6c46893b4f814c
Resurreccion-SamuelAngelo-BSIT32E2 <161041733+Resurreccion-SamuelAngelo-BSIT32E2@users.noreply.github.com>
2024-03-09 02:27:13 +0800

    Add project files.

---
+++ PRELIM_E3_RESURRECCION_BSIT32E2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_E3_RESURRECCION_BSIT32E2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
